299474d51b9c54e6a033a4815e9d814962638c47
Bhaskar Yogi <shardayogi60@gmail.com>
2026-08-07 22:50:03 +0530

    MongoDB & Express Setup

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,15 @@ const express = require('express');//require express
 const cookieParser = require('cookie-parser');//require cookie-parser
 const cors = require('cors');
 const dotenv = require('dotenv');//require dotenv
+const connectDb = require('./config/dbConnect');
 
 dotenv.config();//configure dotenv-this will automaticall featch velues from .env file
 
 const PORT = process.env.PORT;
 const app=express();
+
+//database connection
+connectDb()
 
 app.listen(PORT, ()=>{
     console.log(`server running... on port ${PORT}`);
