5c6cec0c4e724e4295ad9942a540792c2d01bbd6
Bhaskar Yogi <shardayogi60@gmail.com>
2026-08-11 21:01:02 +0530

    Sending OTP to Email & Phone Number feature added

---
+++ backend/controllers/authController.js
@@ -0,0 +1,43 @@
+
+
+//step 1 Send OTP
+
+const User = require("../models/User");
+const otpGenerate = require("../utils/otpGenerater");
+const response = require("../utils/responseHandler");
+
+const sendOtp = async(req,res)=>{
+    const {phoneNumber,phoneSuffix,email}=req.body;
+    const otp=otpGenerate();
+    const expiry= new Date(Date.now()+5*60*1000);
+    let user ;  
+    try {
+        if(email){
+            user= await User.findOne({email})
+
+            if(!User){
+                user = new User({email})
+            }
+            user.emailOtp=otp;
+            user.emailOtpExpiry=expiry;
+            await user.save();
+
+            return response(res,200,'OTP sent to your email',{email})
+        }
+        if(!phoneNumber || !phoneSuffix){
+            return response(res,400,'Phone number and suffix are required')
+        }
+        const fullPhoneNumber=`${phoneSuffix}${phoneNumber}`
+        user=await User.findOne({phoneNumber});
+        if(!user){
+            user= await new User ({phoneNumber,phoneSuffix})
+        }
+
+        await user.save();
+
+        return response(res,200,'otp send successfully',user)
+    } catch (error) {
+        console.error(error);
+        return response(res,500,'Internal server error')
+    }
+}
