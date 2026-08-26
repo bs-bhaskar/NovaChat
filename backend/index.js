e1129cbadc41bb584dfed92801da21b81b835498
Bhaskar Yogi <shardayogi60@gmail.com>
2026-08-26 18:47:50 +0530

    OTP Logic & Verification Flow implemented

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,24 @@ const cookieParser = require('cookie-parser');//require cookie-parser
 const cors = require('cors');
 const dotenv = require('dotenv');//require dotenv
 const connectDb = require('./config/dbConnect');
+const bodyParser=require('body-parser')
+const authRoute = require('./routes/authRoute')
 
 dotenv.config();//configure dotenv-this will automaticall featch velues from .env file
 
 const PORT = process.env.PORT;
 const app=express();
 
+//Middleware
+app.use(express.json())//parse body data
+app.use(cookieParser())//parse token on every request
+app.use(bodyParser.urlencoded({extended:true}))
+
 //database connection
 connectDb()
+
+//Routes
+app.use('/api/auth',authRoute)
 
 app.listen(PORT, ()=>{
     console.log(`server running... on port ${PORT}`);
