1372e93bd6e3d629bf4418a7f4c9ada39c427870
Bhaskar Yogi <shardayogi60@gmail.com>
2026-09-07 22:42:24 +0530

    Auth Middleware & User API

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,6 +8,7 @@ const response = require("../utils/responseHandler");
 const twilloService=require('../services/twilloService.js');
 const generateToken = require("../utils/generateToken.js");
 const { uploadFileToCloudinary } = require("../config/cloudinaryConfig.js");
+const Conversation=require('../models/Conversation.js')
 
 //step 1 Send OTP
 const sendOtp = async(req,res)=>{
@@ -121,8 +122,66 @@ const updateProfile=async(req,res)=>{
     }
 }
 
+const checkAuthenticated=async(req,res)=>{
+    try {
+        const userId=req.user.userId;
+        if(!userId){
+            return response(res,404,'user is unauthenticated ! please login before access our app')
+        }
+        const user=await User.findById(userId)
+        if(!user){
+            return response(res,404, 'user not found');
+        }
+        return response(res,200,'user retrived and allow to use whatsapp', user)
+    } catch (error) {
+        console.error(error);
+        return response(res,500,'Internal server error')
+    }
+}
+
+const logout=(req,res)=>{
+    try {
+        res.cookie("auth_token","",{expires:new Date(0)});
+        return response(res,200,'user logout successfully')
+    } catch (error) {
+        console.error(error);
+        return response(res,500,'Internal server error')
+    }
+}
+
+const getAllUsers=async(req,res)=>{
+    const loggedInUser=req.user.userId
+    try {
+        const users=await User.find({_id:{$ne:loggedInUser}}).select(
+            "username profilePicture lastSeen isOnline about phoneNumber phoneSuffix"
+        ).lean();
+
+        const usersWithConversation=await Promise.all(
+            users.map(async (user)=>{
+                const conversation= await Conversation.findOne({
+                    participants:{$all:[loggedInUser,user?._id]}
+                }).populate({
+                    path:"lastMessage",
+                    select:"content createdAt sender receiver"
+                }).lean();
+                return {
+                    ...user,
+                    conversation:conversation || null
+                }
+            })
+        );
+        response(res,200,'users retrived successfully',usersWithConversation)
+    } catch (error) {
+        console.error(error);
+        return response(res,500,'Internal server error')
+    }
+}
+
 module.exports={
     sendOtp,
     verifyOtp,
-    updateProfile
+    updateProfile,
+    checkAuthenticated,
+    logout,
+    getAllUsers
 }
--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     phoneNumber:{type:String,unique:true,sparse:true},
     phoneSuffix:{type:String,unique:false},
-    userName:{type:String},
+    username:{type:String},
     email:{
         type:String,
         lowercase:true,
